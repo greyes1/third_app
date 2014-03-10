cbf88b8892bfe787d9d8f4a71de01f1dfe3ebf6e
greyes@chicagotechacademy.org <Gabe>
2014-03-10 13:06:22 -0500

    innitcommit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThirdApp::Application.config.secret_token = '9396d529529205fc64370fa4bd36f4a2315236fcb6fa448a3a568d941f36a47f83dd4e8a59cda584ef217c40b8de7b409013d9e3d9d629709d9c9e7b5b4baf3a'
