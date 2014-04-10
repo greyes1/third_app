833bff7bd45065d21d1f30aa3a4296b2ae3dbbbe
Gabriel <greyes@chicagotechacaedmy.org>
2014-04-10 12:56:43 -0500

    added signup page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,37 +1,50 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+
+  subject{page}
+
   describe "Home Page" do
-    it "should have the content Sample App" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/home'
-      page.should have_selector("h1", :text => 'Sample App')
-    end
-    it "should have a little" do
-      visit '/static_pages/home'
-      page.should have_selector("title", :text => 'Home Page')
-    end
+
+    before {visit root_path}
+
+    it {page.should have_selector("h1", text: 'Home Page')}
+    it {page.should have_selector("title", text: 'Third App')}
   end
+
   describe "Help Page" do
-    it "should have the content Help Page" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/help'
-      page.should have_selector("h1", :text => 'Help Page')   
-    end
-    it "should have the content Sample App" do
-      visit '/static_pages/help'
-      page.should have_selector("title", :text => 'Help Page')
-    end
+
+    before {visit help_path}
+
+    it {page.should have_selector("h1", text: 'Help Page')}
+    it {page.should have_selector("title", text: 'Help Page')}
   end
+
+  describe "About Us" do
+    
+    before {visit root_path}
+
+    it {page.should have_selector("h1", text: 'About Us')}
+    it {page.should have_selector("title", text: 'About Us')}
+  end
+
   describe "About Us Page" do
-    it "should have the content About Us" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/about'
-      page.should have_selector("h1", :text => 'About Page')
-    end
-    it "should have the content Sample App" do
-      visit '/static_pages/about'
-      page.should have_selector("title", :text => 'About Page')    
-    end
+
+    before {visit contact_path}
+
+    it {page.should have_selector("h1", text: 'Contact Us')}
+    it {page.should have_selector("title", text: 'Contact Us')} 
+  end
+  it "should have the correct links" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text: 'About Us'
+    click_link "Help"
+    page.should have_selector 'title', text: 'Help'
+    click_link "Contact"
+    page.should have_selector 'title', text: 'Contact'
+    click_link "Home"
+    click_link "Sign up now!"
+    page.should have_selector 'title', text: 'Sign up'
   end
 end
